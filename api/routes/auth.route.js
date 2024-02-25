fb3ced1134ed42fb1f2ceca1325eed47489d23d5
Sunil Gopal C V <sunilgopal63@gmail.com>
2024-02-25 11:24:58 +0530

    create signup api route

---
+++ api/routes/auth.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { signup } from "../controllers/auth.controller.js";
+
+const route = express.Router();
+
+route.post("/signup", signup);
+
+export default route;
