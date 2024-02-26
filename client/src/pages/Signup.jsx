09fb0647f25a752c7c6cf1fa5ea10c67c16a36d7
Sunil Gopal C V <sunilgopal63@gmail.com>
2024-02-26 11:31:05 +0530

    Complete signup page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,45 @@
 import COVER_IMAGE from "/images/Signup.jpg";
 import Logo from "./components/Logo";
 import GOOGLE_ICON from "/icons/LogoGoogle.png";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success == false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/login");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
     <div className="flex flex-col md:flex-row h-[85vh] align-middle items-center">
       <div className="flex flex-col align-middle items-center">
@@ -25,31 +63,43 @@ export default function Signup() {
             Sign up today to discover endless recipes, share your favorites, and
             connect with fellow food enthusiasts!
           </p>
-          <form className="space-y-4 text-primary font-proxima-nova">
+          <form
+            onSubmit={handleSubmit}
+            className="space-y-4 text-primary font-proxima-nova"
+          >
             <div>
               <input
+                id="username"
                 type="text"
                 className="w-full py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none font-overlock"
+                onChange={handleChange}
                 placeholder="Username"
               />
             </div>
             <div>
               <input
+                id="email"
                 type="email"
                 className="w-full py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none font-overlock"
+                onChange={handleChange}
                 placeholder="Email"
               />
             </div>
             <div>
               <input
+                id="password"
                 type="password"
                 className="w-full py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none font-overlock"
+                onChange={handleChange}
                 placeholder="Password"
               />
             </div>
-            <div className="w-full flex flexcol my-2">
-              <button className="w-full text-white bg-[#060606] rounded-md my-1 p-4 font-proxima-nova font-semibold text-center flex items-center justify-center">
-                Sign Up
+            <div className="w-full flex flex-col my-2">
+              <button
+                disabled={loading}
+                className="w-full text-white bg-[#060606] rounded-md my-1 p-4 font-proxima-nova font-semibold text-center flex items-center justify-center hover:bg-[#292929]"
+              >
+                {loading ? "Loading..." : "Sign-up"}
               </button>
             </div>
             <button className="w-full text-[#060606] bg-white border-2 border-black rounded-md py-2 font-proxima-nova font-semibold text-center flex items-center justify-center">
@@ -60,9 +110,12 @@ export default function Signup() {
           <div className="w-full">
             <p className="text-sm font-proxima-nova text-primary">
               Already a member?
-              <span className="font-semibold text-blue-500 underline underline-offset-2 cursor-pointer px-1">
-                Login
-              </span>
+              <Link to={"/login"}>
+                <span className="font-semibold text-blue-500 underline underline-offset-2 cursor-pointer px-1">
+                  Login
+                </span>
+              </Link>
+              {error && <span className="text-red-500">{error}</span>}
             </p>
           </div>
         </div>
