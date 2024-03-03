bd6735f8740b79cdd42d0a228c33517c127775d0
Sunil Gopal C V <sunilgopal63@gmail.com>
2024-03-03 19:55:40 +0530

    add delete user functionality

--- client/src/pages/components/Dropdown.jsx
+++ client/src/pages/components/Dropdown.jsx
@@ -1,9 +1,32 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+} from "../../redux/user/userSlice";
 
 export default function Dropdown() {
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   if (currentUser) {
+    const handleDeleteUser = async () => {
+      try {
+        dispatch(deleteUserStart());
+        const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (data.success === false) {
+          dispatch(deleteUserFailure(data.message));
+          return;
+        }
+        dispatch(deleteUserSuccess(data));
+      } catch (error) {
+        dispatch(deleteUserFailure(error.message));
+      }
+    };
     return (
       <>
         <Link
@@ -18,6 +41,12 @@ export default function Dropdown() {
         >
           Log Out
         </Link>
+        <span
+          className="block px-4 py-2  hover:bg-gray-200 no-underline text-primary font-proxima-nova"
+          onClick={handleDeleteUser}
+        >
+          Delete Account
+        </span>
       </>
     );
   } else {
